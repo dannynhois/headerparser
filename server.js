2287b4bf2dc250390e94d608e7b8f050786e5b58
Danny Nhoisaykham <dannynhois@gmail.com>
2017-04-16 19:40:30 +0000

    add ip address

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req,res){
-    res.send("header parser");
+    //get ip address
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    
+    
+    res.send("ip: " + ip);
 })
 
 app.listen(8080, function(){
