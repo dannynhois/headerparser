5290c60f9d4af2b9104a66bfef4d3ff3664d6fc9
Danny Nhoisaykham <dannynhois@gmail.com>
2017-04-16 20:16:20 +0000

    completed project

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req,res){
-    //get ip address
+    //get ip address, language, and system information
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     
+    //get operating system
+    var os = req.headers['user-agent'];
+    os = os.split(/[\(\)]/)[1];
     
-    res.send("ip: " + ip);
+    //get language
+    var language = req.headers['accept-language'].split(',')[0];
+
+    //create json file
+    var data = {
+        'ipaddress':ip,
+        'language':language,
+        'software':os
+    };
+    
+    //return json file
+    res.send(data);
 })
 
 app.listen(8080, function(){
